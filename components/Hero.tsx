67f93732aaa03b7d604511ab63caadb632e8ce3b
Prophet Bestman <bestiproph@gmail.com>
2023-05-10 10:11:01 +0100

    Updated hero text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,6 +7,7 @@ import {
 import React from "react";
 import { Typewriter } from "react-simple-typewriter";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -43,7 +44,7 @@ const Hero = () => {
             <p className="font-normal md:font-medium text-base md:text-lg max-w-2xl h-[180px] md:h-[130px]">
               <Typewriter
                 words={[
-                  "Hello, I'm Prophet Ogwuche, an experienced React.js frontend developer with a strong skill set in HTML, CSS, JavaScript, TypeScript, Next.js, GraphQL, React-Query, Redux-toolkit, Webpack, and a vast list of React-related libraries",
+                  "Hello, I'm Prophet Bestman, an experienced React.js frontend developer with a strong skill set in HTML, CSS, JavaScript, TypeScript, Next.js, GraphQL, React-Query, Redux-toolkit, Webpack, and a vast list of React-related libraries",
                   "I've had the privilege of contributing to the development of large-scale enterprise applications for various organizations, including banks, using APIs such as Google Maps, Google Places, and Paystack. My experience in utilizing these APIs has given me a strong understanding of how to integrate third-party services into web applications seamlessly.",
                   "As a frontend developer, I am passionate about creating intuitive and engaging user interfaces that enhance the user experience. My expertise in React.js and related technologies enables me to build responsive, scalable, and performant web applications that meet the needs of clients and end-users.",
                   "Take a look at my portfolio to see some of the web applications that I've worked on. I'm always eager to take on new challenges, and I look forward to collaborating with you to create outstanding digital experiences for your users. Let's build something great together!",
@@ -51,13 +52,14 @@ const Hero = () => {
                 loop={100}
                 cursor
                 cursorStyle="_"
-                typeSpeed={15}
-                deleteSpeed={5}
-                delaySpeed={4000}
+                typeSpeed={5}
+                deleteSpeed={4}
+                delaySpeed={15000}
               />
             </p>
-            <button
-              className="
+            <Link href="/portfolio">
+              <button
+                className="
                       text-darkShade 
                       mt-[56px] px-[75px] py-4 
                       bg-offWhite rounded-[20px]
@@ -65,17 +67,12 @@ const Hero = () => {
                       transition-all ease-in-out duration-300
                       w-fit
                       "
-            >
-              <p className=" text-lg font-semibold opacity-75 "></p>
-              View Portfolio
-            </button>
+              >
+                <p className=" text-lg font-semibold opacity-75 "></p>
+                View Portfolio
+              </button>
+            </Link>
           </motion.div>
-
-          {/* <p className="font-normal md:font-medium text-base md:text-lg">
-            Some--kinda text that says something smart Some--kinda text that
-            says something smart Some--kinda text that says something smart and
-            some more stuffs for you
-          </p> */}
         </div>
         <div className="col-span-1 lg:col-span-5 h-[450px] sm:h-[512px] relative order-1 lg:order-2">
           <motion.div
