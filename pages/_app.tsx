66fb3880b4029fbcf5a2192b3871a651611379d6
Prophet Bestman <bestiproph@gmail.com>
2023-10-16 13:29:52 +0100

    feat: added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import NavProvider from "@/contexts/NavContext";
-import { MainLayout } from "@/layout";
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "react-modern-drawer/dist/index.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+
+import NavProvider from "@/contexts/NavContext";
+import { MainLayout } from "@/layout";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const client = new QueryClient();
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/inline-script-id */
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,6 +9,21 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <Script strategy="lazyOnload">
+          {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+        </Script>
       </body>
     </Html>
   );
